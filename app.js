dbd17ac699dc2079d61d4d24d3033a719b63decf
Amanda Overmyer <aovermyerphoto@gmail.com>
2016-07-14 19:25:00 -0400

    styled sheet and added delete all button

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
 $(document).ready(function() {
-	
+	var count = 0;
+
 	function addItem() {
 		var item = $(".newItem").val();
-		$(".items").append('<li><input class="listItem" type="checkbox"/>' + item + '<button class="remove">x</button></li>');
+		$(".items").append('<li><input class="checkboxid" type="checkbox"/>' + item + '<button class="remove">x</button></li>');
 		$('.newItem').val('');
+		count=count + 1;
+		$('.count').text(count);
 	}
 
 	function onKeyDown(event) {
@@ -13,18 +16,36 @@ $(document).ready(function() {
 		}
 
 	}
-	/*$(".items").change(function() {
-		$(this).toggleClass('checked');
+	
+	$(".items").on('click', '.checkboxid' , function() {
+		$(this).parent().toggleClass('checked');
+		var selected = $(':checked').length;
+		if (selected === 0){
+			$('.delete-checked').css('display', 'none');
+		} else {
+			$('.delete-checked').css('display', 'block');
+		}
+
 	});
-	*/
+
+
 
 
 	$(".items").on("click", ".remove", function() {
-		$(this).parent('li').remove();
+		console.log(this);
+		$(this).parent().remove();
+		count=count - 1;
+		$('.count').text(count);
 	});
 
 
-	
+	$('.delete-checked').click(function() {
+		var selected = $(':checked').parent();
+		count = count - selected.length;
+		selected.remove();
+		$('.count').text(count);
+	})
+
 	$('.addItem').click(addItem);
 	$('.newItem').keydown(onKeyDown);
 
